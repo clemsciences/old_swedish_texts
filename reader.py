6645f287bfd00314329948fcbe7775d98ab2ac7b
clemsciences <clemsciences@aol.com>
2018-07-08 00:47:27 +0200

    read corpora from spraakbanken

--- reader.py
+++ reader.py
@@ -1,6 +1,45 @@
+"""
+
+"""
+
 from bs4 import BeautifulSoup
+from xml.etree import ElementTree
+import codecs
 
-with open('texts/moses.html', 'r') as f:
-    b = BeautifulSoup(f.read(), 'html5lib')
+from utils import normalize_sentence
 
-print(b.text[1000:2100])
+__author__ = ["Clément Besnier <clemsciences@aol.com>", ]
+
+
+def read_moses():
+    with open('texts/moses.html', 'r') as f:
+        b = BeautifulSoup(f.read(), 'html5lib')
+
+    print(b.text[1000:2100])
+
+
+def read_corpus():
+    with codecs.open("corpora/fsv-aldrelagar.xml", "r", encoding="utf8") as f:
+        text = f.read()
+
+    root = ElementTree.fromstring(text)
+    ltext = []
+    for text in root:
+        title = text.attrib["title"]
+        print(title)
+        lparagraph = []
+        for paragraph in text:
+            sentences = []
+            for sentence in paragraph:
+                lsentence = []
+                for word in sentence:
+                    lsentence.append(word.text)
+                sentences.append(normalize_sentence(lsentence))
+            lparagraph.append(" ".join(sentences))
+        # print("\n\n".join(lparagraph))
+        ltext.append("\n\n".join(lparagraph))
+    return ltext
+
+
+if __name__ == "__main__":
+    print(read_corpus()[0])
